bedeba56f1894a1c8670a6fc317e0dd31d1f7901
Oleh <olegkaraaa@gmail.com>
2021-08-04 23:38:24 +0300

    task-2-done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
+
+const list = document.querySelector("ul#ingredients");
+
+ingredients.forEach((el) => {
+  const li = document.createElement("li");
+  li.textContent = el;
+  list.append(li);
+});
